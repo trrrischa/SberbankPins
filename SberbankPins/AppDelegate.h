15d70e78c6199d2300228dcea72d1476d1042524
Ekaterina <trrrischa@macbook-pro-ekaterina.local>
2018-02-16 20:33:01 +0500

    Initial Commit

---
+++ SberbankPins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SberbankPins
+//
+//  Created by Ekaterina Nesterova on 16.02.2018.
+//  Copyright © 2018 Ekaterina Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SberbankPins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SberbankPins
+//
+//  Created by Ekaterina Nesterova on 16.02.2018.
+//  Copyright © 2018 Ekaterina Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
